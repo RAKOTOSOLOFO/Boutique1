c3640e1b039cd12d3648f99c941d3034b0387cfd
iandry <ian_rak@yahoo.fr>
2017-03-16 09:31:22 +0100

    panier

--- static/js/panier.js
+++ static/js/panier.js
@@ -1,39 +1,297 @@
 $(document).ready(function() {
+         var GET_PARAM = function(name) {
+         return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+     };
 
-    var panier = JSON.parse(sessionStorage.getItem("panier"));
-    for (var index in panier){
-        var produit = catalog[index]
-        var quantite = panier[index];
+     function genProduct(dico, id, isProductPage) {
 
-        var main = $(".panier");
-        var sectionProduit = $("<section></section>");
-        var divRow = $("<div class='row'></div>");
-        var divCol = $("<div class='col-xs-12 textaligncenter'></div>");
-        var photo = $("<img>");
-        var titre = $("<h2></h2>");
-        var description = $("<p></p>");
-        var sectionPrix = $("<section></section>");
-        var spanPrix = $("<span></span>");
-        var spanTVA = $("<span></span>");
-        var spanTotal = $("<span></span>");
+         var img = $('<img>').attr({
 
-        sectionProduit.appendTo(main);
-        divRow.appendTo(sectionProduit);
-        divCol.appendTo(divRow);
-        photo.appendTo(divCol);
-        photo.attr("src", catalog[index].thumb);
-        titre.appendTo(divCol);
-        titre.html(catalog[index].name);
-        description.appendTo(divCol);
-        description.html(catalog[index].description);
-        sectionPrix.appendTo(main);
-        spanPrix.appendTo(sectionPrix);
-        spanPrix.html(catalog[index].price);
-        spanTVA.appendTo(sectionPrix);
-        spanTVA.html("10%");
-        // spanTotal.appendTo(sectionPrix);
-        // spanTotal.html(catalog[index].price.val() + spanTVA );
-    }
+             src: dico.thumb,
 
+             alt: "Item thumb"
 
-});
+         });
+
+     ​
+
+         var title = $('<h3>').html(dico.name);
+
+         var description = $('<p>').html(dico.description);
+
+         var firstDiv = $('<div>').append(img, title, description);
+
+     ​
+
+         if (!isProductPage) {
+
+             var linkVoir = $('<a>').attr({
+
+                 'class': 'btn btn-default',
+
+                 href: 'produit.html?id='+ id,
+
+             }).html('Voir');
+
+     ​
+
+         } else {
+
+             var linkVoir = $('<a>').attr({
+
+                 'class': 'btn btn-default',
+
+                 href: 'catalogue.html',
+
+             }).html('Retour catalogue');
+
+         }
+
+     ​
+
+         var addCart = $('<button>').attr({
+
+             'class': 'btn btn-default btn-panier',
+
+             'id': id
+
+         }).html('Ajouter au panier')
+
+         .popover({
+
+             content: 'Ce produit à été ajouté au panier.',
+
+             placement: 'bottom',
+
+             trigger: 'manual'
+
+         })
+
+         .click(function() {
+
+             addToLocalStorage($(this).attr('id'));
+
+             $(this).popover('show');
+
+             $(this).addClass('popover_tmp');
+
+             setTimeout(function() {
+
+                 $('.popover_tmp').popover('hide');
+
+                 $('.popover_tmp').removeClass('popover_tmp');
+
+             }, 1500);
+
+     ​
+
+         });
+
+     ​
+
+         var price = $('<span>').html('Prix : ' + dico.price + '€');
+
+         var lastDiv = $('<div>').append(linkVoir, addCart, price);
+
+     ​
+
+         return $('<article>').append(firstDiv, lastDiv);
+
+     }
+
+     ​
+
+     function displayProducts(products, startIndex) {
+
+         $('article').parent().parent().remove();
+
+     ​
+
+         var i = startIndex;
+
+         var max = startIndex+10;
+
+         var container = $('main > section');
+
+     ​
+
+         while (i<products.length && i<max) {
+
+             var j=0;
+
+             var row = $('<div>').addClass('row');
+
+     ​
+
+             while (j<2 && i < products.length) {
+
+                 var col = $('<div>').addClass('col-md-6');
+
+                 var productId = products[i].name.split(' ')[1];
+
+                 col.append(genProduct(products[i], productId, false));
+
+                 row.append(col);
+
+                 j++;
+
+                 i++;
+
+             }
+
+     ​
+
+             container.append(row);
+
+         }
+
+     ​
+
+         if (products.length > 10) {
+
+             setupPagination(startIndex/10, products.length);
+
+         }
+
+     }
+
+     ​
+
+     var GET_PARAM = function(name) {
+
+         return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+
+     };
+
+     ​
+
+     function setToLocalStorage(id, qty) {
+
+         var panier = localStorage.getItem('panier');
+
+     ​
+
+         if (panier == null) {
+
+             panier = {};
+
+             panier[id] = qty;
+
+             localStorage.setItem('panier', JSON.stringify(panier));
+
+     ​
+
+         } else {
+
+             panier = JSON.parse(panier);
+
+             panier[id] = qty;
+
+             localStorage.setItem('panier', JSON.stringify(panier));
+
+         }
+
+         updatePanierCount();
+
+     }
+
+     ​
+
+     function addToLocalStorage(id) {
+
+         var panier = localStorage.getItem('panier');
+
+     ​
+
+         if (panier == null) {
+
+             panier = {};
+
+             panier[id] = 1;
+
+             localStorage.setItem('panier', JSON.stringify(panier));
+
+     ​
+
+         } else {
+
+             panier = JSON.parse(panier);
+
+             if (panier[id]) {
+
+                 panier[id] = parseInt(panier[id]) + 1;
+
+     ​
+
+             } else {
+
+                 panier[id] = 1;
+
+             }
+
+             localStorage.setItem('panier', JSON.stringify(panier));
+
+         }
+
+         updatePanierCount();
+
+     }
+
+     ​
+
+     function removeFromLocalStorage(id) {
+
+         var panier = localStorage.getItem('panier');
+
+         panier = JSON.parse(panier);
+
+         delete panier[id];
+
+         localStorage.setItem('panier', JSON.stringify(panier));
+
+         updatePanierCount();
+
+     }
+
+     ​
+
+     function updatePanierCount() {
+
+         var total = 0;
+
+         var panier = localStorage.getItem('panier');
+
+         if (panier != null) {
+
+             panier = JSON.parse(panier);
+
+             for (var id in panier) {
+
+                 total += parseInt(panier[id]);
+
+                 console.log(total);
+
+             }
+
+         }
+
+         $('#item_count').html('( ' + total + ' )');
+
+     }
+
+     ​
+
+     $(document).ready(function() {
+
+         updatePanierCount();
+
+     });
+
+     rakotosolofo
+     9:46 PM
+     Merci Quentin.
+     quentin_gautreau
+     9:09 AM
+     referentiel.docx
+
+     Word Document Click to download
